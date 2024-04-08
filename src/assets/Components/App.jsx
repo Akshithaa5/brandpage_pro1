7c941e41ac56b12265e2d6b558f8de7bd758dd9e
Akshithaa5 <jessierowe143@gmail.com>
2024-04-08 17:47:29 +0530

    Brandpage project_1

---
+++ src/assets/Components/App.jsx
@@ -0,0 +1,18 @@
+
+
+import "/.App.css/";
+import Navigation from "./Navigation";
+import HeroSection from "./Hero";
+
+const App = () => {
+    return (
+
+        <div>
+            <Navigation />
+            <HeroSection />
+        </div>
+    );
+};
+
+export default App;
+
---
+++ src/assets/Components/Hero.jsx
@@ -0,0 +1,35 @@
+const HeroSection = () => {
+
+    return(
+        <main className = "hero container">
+            <div className="hero-content">
+<h1> YOUR FEET DESRVE THE BEST </h1>
+<p> YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+    YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES. </p>
+
+    <div className = "hero-btn">
+        <button> Shop Now </button>
+        <button className= "secondary-btn" > Category </button>
+            </div>
+            <div className = "Available">
+<p>Also Available On</p>
+
+
+
+<div className = "brand-icons">
+<img src ="/Images/amazon.png" alt = "amazon-logo" /> 
+<img src ="/Images/flipkart.png" alt = "flipkart-logo" /> 
+</div>
+
+            </div>
+        </div>
+
+        <div className = "hero-image"></div>
+        <img src ="/Images/pumas.png" alt = "shoe-logo" /> 
+
+        </main>
+    );
+
+};
+
+export default HeroSection;
